bfb67a8579183da1fa5a23fb19fb32353a4a803f
DanielFelipeDeveloper <danielfelipedeveloper@gmail.com>
2020-06-04 11:25:59 -0400

    Criando e iniciando servidor

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const server = express()
+
+
+// configurar caminhos da minha aplicação
+// página inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req, res) => {
+    res.send("Cheguei aqui")
+})
+
+// ligar o servidor
+server.listen(3000) // função que ouve a porta 3000
