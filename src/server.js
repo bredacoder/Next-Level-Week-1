8330a57af172ae883116694b8baf11df731db267
Daniel Felipe <danielfelipedeveloper@gmail.com>
2020-06-04 19:52:27 -0400

    NunJucks Include

--- src/server.js
+++ src/server.js
@@ -4,20 +4,30 @@ const server = express()
 // configurar pasta pública
 server.use(express.static("public"))
 
+// Template Engine
+const nunjucks = require("nunjucks");
+/*                 pastas do html*/  
+nunjucks.configure("src/views", {
+    express: server, // liga o nunjucks ao express
+    noCache: true
+})
+
+
 // configurar caminhos da minha aplicação
 // página inicial
 // req: Requisição
 // res: Resposta
 server.get("/", (req, res) => { // configuração de rotas
-    res.sendFile(__dirname + "/views/index.html")
+// Retorna a renderização da página/rota após passa-la pelo motor do nunjucks
+    return res.render("index.html", { title: "Um título" }) 
 })
 
 server.get("/create-point", (req,res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
 })
 
-server.get("/search-results", (req,res) => {
-    res.sendFile(__dirname + "/views/search-results.html")
+server.get("/search", (req,res) => {
+    return res.render("search-results.html")
 })
 // ligar o servidor
 server.listen(3000) // função que ouve a porta 3000
