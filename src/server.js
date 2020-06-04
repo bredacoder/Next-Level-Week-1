6811663af474c40e81c0ba8871ba17b299fe5691
DanielFelipeDeveloper <danielfelipedeveloper@gmail.com>
2020-06-04 15:12:37 -0400

    Configurando arquivos estáticos e rotas

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 const express = require("express")
 const server = express()
 
+// configurar pasta pública
+server.use(express.static("public"))
 
 // configurar caminhos da minha aplicação
 // página inicial
 // req: Requisição
 // res: Resposta
-server.get("/", (req, res) => {
-    res.send("Cheguei aqui")
+server.get("/", (req, res) => { // configuração de rotas
+    res.sendFile(__dirname + "/views/index.html")
 })
 
+server.get("/create-point", (req,res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+server.get("/search-results", (req,res) => {
+    res.sendFile(__dirname + "/views/search-results.html")
+})
 // ligar o servidor
 server.listen(3000) // função que ouve a porta 3000
